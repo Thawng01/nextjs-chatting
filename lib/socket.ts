eb5cad5f6f0e8a954fdd7abfb3e3fb93bbca170a
thechin press <thechinpress@gmail.com>
2024-01-18 22:18:52 +0630

    fixed

--- lib/socket.ts
+++ lib/socket.ts
@@ -1,3 +1,3 @@
 import io from 'socket.io-client'
 
-export const socket = io('http://localhost:9000', { autoConnect: false })
+export const socket = io('https://chatting-api-ks84.onrender.com', { autoConnect: false })
