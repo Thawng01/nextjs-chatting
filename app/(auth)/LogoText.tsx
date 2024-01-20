5ad01629cde41d410e580039b899c35a6206d311
thechin press <thechinpress@gmail.com>
2024-01-20 22:48:12 +0630

    fixed auth layout

--- app/(auth)/LogoText.tsx
+++ app/(auth)/LogoText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LogoText = () => {
     return (
-        <h1 className="font-semibold md:text-3xl md:font-bold">
+        <h1 className="font-semibold text-lg md:text-3xl md:font-bold">
             Stay connected with <br />
             <span className="text-sky-600">family, friends and people</span> you
             love aroung the world.
--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -7,7 +7,7 @@ const AuthLayout = ({ children }: { children: React.ReactNode }) => {
         <div className="flex flex-col md:flex-row w-11/12 lg:w-8/12 mx-auto h-screen items-center">
             <AuthLogo />
             {children}
-            <div className="block md:hidden mt-5 mb-3 text-center">
+            <div className="block md:hidden mt-10 mb-3 text-center">
                 <LogoText />
             </div>
         </div>
